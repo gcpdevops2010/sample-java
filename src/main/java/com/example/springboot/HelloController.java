a510ad75dfceda8a73e6ea68cb5a649036e0d08e
gcpdevops2010 <149203598+gcpdevops2010@users.noreply.github.com>
2023-11-15 04:42:39 -0800

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,8 +8,12 @@ public class HelloController {
 	String CONSTANT="TEST";
 	@GetMapping("/")
 	public String index() {
+		String name1 = "This is test";
+		String name2 = "This is test";
+		String name3 = "This is test";
+		
 		CONSTANT="TEST2";
-		return "Greetings from Spring Boot!" ;
+		return "Greetings from Spring Boot!" + name1 + name2 + name3 ;
 	}
 
 }
